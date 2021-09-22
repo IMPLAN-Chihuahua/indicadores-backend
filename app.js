46621f23f258bc6f82305114ccac808b2b28c59b
Luis Cruz <luisccc99@gmail.com>
2021-09-22 05:54:47 -0600

    Add response to simple route

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8081;
 
+app.get('/', (req, res) => {
+    res.json({ msg: "foobar" });
+})
+
 app.listen(port, () => {
     console.log(`app starting at port ${port}`);
 });
