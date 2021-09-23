151a06b74666d8ce2c147eb7ea2c231ecf61d830
Luis Cruz <luisccc99@gmail.com>
2021-09-23 00:45:09 -0600

    Create file to connect to db

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
 const port = 8081;
+const { sequelize } = require('./config/db');
+require('dotenv').config();
+
+console.log(sequelize.config);
+sequelize.authenticate()
+    .then(() => console.log('connection has been established successfully'))
+    .catch(err => console.error(err));
 
 app.get('/', (req, res) => {
     res.json({ msg: "test" });
