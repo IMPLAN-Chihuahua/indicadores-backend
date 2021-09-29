36b5aa5225dcd61eb89118979f68cfbfca447057
Luis Cruz <luisccc99@gmail.com>
2021-09-28 20:24:10 -0600

    Configure db file to connect to postgre database

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const { DB_NAME, DB_USER, DB_PASSWORD, DB_HOST, DB_PORT } = process.env;
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
-    dialect: 'mssql',
+    dialect: 'postgres',
     port: DB_PORT
 });
 
