9c8bfc08d85c12f1b8a216dfb1fab7c70af15124
Luis Cruz <luisccc99@gmail.com>
2021-10-18 21:49:30 -0600

    add validation of body with express validator

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,34 +1,34 @@
-// TODO: Validar datos con joi o express-validator
-// TODO: decidir que paquete utilizar brcypt o bcryptjs
-
 const { Usuario } = require('../models/usuario');
 const bcrypt = require('bcrypt');
 require('dotenv').config();
 const { TOKEN_SECRET } = process.env;
 const jwt = require('jsonwebtoken');
+const { validationResult } = require('express-validator');
 
 const login = async (req, res) => {
 
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+    
     // obtener correo y clave de los parametros del body
     const { correo, clave } = req.body;
 
-    // validacion basica
-    if (!correo || !clave) {
-        return res.status(400).json({
-            message: 'Email and password are required'
+    const existingUser = await Usuario.findOne({ where: { correo: correo } });
+
+    if (existingUser && existingUser.activo === 'NO') {
+        return res.status(403).json({
+            msg: "This account is not active"
         });
     }
 
-    const existingUser = await Usuario.findOne({ where: { correo: correo } });
-
     // await de bcrypt.compare() regresa un boolean
-    if (existingUser
-        && (await bcrypt.compare(clave, existingUser.clave))) {
-
+    if (await bcrypt.compare(clave, existingUser.clave)) {
         const token = jwt.sign(
             { usuario_id: existingUser.id, correo: existingUser.correo },
             TOKEN_SECRET,
-            { expiresIn: '2h' }
+            { expiresIn: '10s' }
         );
         return res.status(200).json({ token: token });
     } else {
