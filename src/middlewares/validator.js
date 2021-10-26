554d41522cf49b760cd4a0733e60bfbe8516f126
Luis Cruz <luisccc99@gmail.com>
2021-10-25 23:22:46 -0600

    add validation of parameters to routes (login, register)

---
+++ src/middlewares/validator.js
@@ -0,0 +1,69 @@
+const { check, validationResult, } = require('express-validator');
+
+const loginValidationRules = () => {
+    return [
+        // correo tenga patron correcto
+        check('correo')
+            .isEmail()
+            .withMessage('El correo tiene un formato incorrecto'),
+
+        // valida que la clave exista y que tenga al menos 8 caracteres
+        check('clave')
+            .isLength({ min: 8 })
+            .withMessage('La clave debe tener al menos 8 caracteres'),
+    ];
+};
+
+const registerValidationRules = () => {
+    return [
+        // correo tenga patron correcto
+        check('correo')
+            .isEmail()
+            .withMessage('El correo tiene un formato incorrecto'),
+
+        // valida que la clave exista y que tenga al menos 8 caracteres
+        // y que cumpla con un formato
+        check('clave')
+            .isLength({ min: 8 })
+            .withMessage('La clave debe tener al menos 8 caracteres'),
+
+        // nombres se encuentra en la peticion
+        check('nombres')
+            .exists()
+            .withMessage('por favor agrega un nombre'),
+
+        // apellido paterno se encuentra en la peticion
+        check('apellidopaterno')
+            .exists()
+            .withMessage('por favor agrega apellido paterno'),
+
+        // 
+    ];
+};
+
+// funcion que hace la validacion (hubo errores en la peticion?)
+const validate = (req, res, next) => {
+    const errors = validationResult(req);
+
+    // si no hubo errores pasar a la siguiente funcion
+    if (errors.isEmpty()) {
+        return next();
+    }
+
+    // dar formato a errores
+    const extractedErrors = [];
+    errors.array().map(error => {
+        extractedErrors.push({ [error.param]: error.msg });
+    });
+
+    return res.status(422).json({
+        errors: extractedErrors
+    });
+
+}
+
+module.exports = {
+    loginValidationRules,
+    registerValidationRules,
+    validate,
+}
