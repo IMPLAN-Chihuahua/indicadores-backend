94b0b1c5b9c93bf957020d6c76cde1ac98bbe7de
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-01-18 23:58:17 -0700

    Using catalog queries as promises instead of async functions

--- src/controllers/catalogoController.js
+++ src/controllers/catalogoController.js
@@ -1,15 +1,62 @@
 const { Ods, CoberturaGeografica, Fuente } = require('../models');
 
 
+
+    
 const getOds = async (_, res) => {
-    try {
-     //   const cobertura = await CoberturaGeografica.findAll();
-        const ods = await Ods.findAll();
-        return res.status(200).json({ ods: ods });
-    } catch(err) {
-        console.log(err);
-        return res.status(500);
-    }
+    //const getOds = Ods.findAll();
+
+    const Promises = [
+        new Promise ( (resolve, reject) => {
+            const getCobertura = CoberturaGeografica.findAll();
+            if(getCobertura === null){
+                reject('Error al obtener las coberturas');
+                return;
+            }
+            resolve(getCobertura);
+        }),
+        new Promise ( (resolve, reject) => {
+            const getOds = Ods.findAll();
+            if(getOds === null){
+                reject('Error al obtener las coberturas');
+                return;
+            }
+            resolve(getOds);
+        }),
+    ]
+
+
+    // const odsPromise = new Promise((resolve, reject) => {
+    //     return( Ods.findAll()
+    //         ? resolve(getOds)
+    //         : reject(new Error('Error al obtener los Ods')));
+    // });
+
+    // const coberturaPromise = new Promise((resolve, reject) => {
+    //     return (CoberturaGeografica.findAll()
+    //         ? resolve(getCoberturas)
+    //         : reject(new Error('Error al obtener los Ods')));
+    // });
+
+    const results = await Promise.all(Promises.map(p => p.catch(e => e)));
+    const validResults = results.filter(result => result !== null);
+    const setted = await Promise.allSettled(Promises);
+    const validSetted = setted.filter(result => result !== null);
+    //console.log(validSetted);
+    //console.dir(validResults[0]);
+
+    return (
+        Promise 
+        .all([Promises[0], Promises[1]])
+        .then(result => {
+            return res.status(200).json({ods:result[0], coberturas:result[1]});
+        })
+        .catch(err => {
+            console.log(err + 'ERROR');
+        })
+    );
+
+
 };
 
 
