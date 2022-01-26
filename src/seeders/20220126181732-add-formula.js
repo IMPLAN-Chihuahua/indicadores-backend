d781a17cfb82e252130c524ac73b32a697c144d9
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-01-26 11:49:05 -0700

    Fixed seed validations on variables

--- src/seeders/20220126181732-add-formula.js
+++ src/seeders/20220126181732-add-formula.js
@@ -2,11 +2,14 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const date = new Date();
     const formula = [{
       id: 1,
       ecuacion: 'Z=x^2 + y^2',
-      descripción: 'Ecuación estándar que sirve para calcular el resultado de la suma de dos números reales equivalentes a algo',
+      descripcion: 'Ecuación estándar que sirve para calcular el resultado de la suma de dos números reales equivalentes a algo',
       idIndicador: 1,
+      createdAt: date,
+      updatedAt: date,
     }];
     await queryInterface.bulkInsert('Formulas', formula, {});
   },
--- src/seeders/20220126182123-add-variables.js
+++ src/seeders/20220126182123-add-variables.js
@@ -5,26 +5,42 @@ module.exports = {
     const variables = [];
     const variablesFromFormula = ['x', 'y', 'z'];
     const nombresFromFormula = ['Descripción de valor 1', 'Descripción de valor 2', 'Descripción de valor 3'];
-    for (let i = 0; i < 2; i++) {
     variables.push({
-      id: i + 1,
-      nombre: variablesFromFormula[i],
+      id: (1),
+      nombre: variablesFromFormula[0],
       codigoAtributo: '001',
-      nombreAttributo: nombresFromFormula[i],
+      nombreAtributo: nombresFromFormula[0],
+      dato: '123',
+      anio: 2021,
+      idUnidad: 1,
+      idFormula: 1,
+    });
+    variables.push({
+      id: (2),
+      nombre: variablesFromFormula[1],
+      codigoAtributo: '002',
+      nombreAtributo: nombresFromFormula[1],
+      dato: '123',
+      anio: 2021,
+      idUnidad: 1,
+      idFormula: 1,
+    });
+    variables.push({
+      id: (3),
+      nombre: variablesFromFormula[2],
+      codigoAtributo: '003',
+      nombreAtributo: nombresFromFormula[2],
       dato: '123',
       anio: 2021,
       idUnidad: 1,
       idFormula: 1,
     })
-  }
+
+
+    await queryInterface.bulkInsert('Variables', variables, {});
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Variables', null, {});
   }
 };
