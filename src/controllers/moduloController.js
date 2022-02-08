d3f5b59a84e06adf7a3df6c1edf2bb768c62b0d6
Luis Cruz <luisccc99@gmail.com>
2022-02-08 14:03:13 -0700

    Add number of indicadores to each modulo in data response of GET /modulos

--- src/controllers/moduloController.js
+++ src/controllers/moduloController.js
@@ -1,12 +1,29 @@
-const { Modulo } = require('../models');
+const { Modulo, Indicador, Sequelize } = require('../models');
 
 const getModulos = async (req, res) => {
     try {
-        const modulos = await Modulo.findAll();
+        const modulos = await Modulo.findAll({
+            where: {
+                activo: 'SI'
+            },
+            attributes: [
+                'id',
+                'temaIndicador',
+                'codigo',
+                'urlImagen',
+                'color',
+                [Sequelize.fn('COUNT', Sequelize.col('Indicadors.id')), 'indicadoresCount']
+            ],
+            include: [{
+                model: Indicador, attributes: []
+            }],
+            group: ['Modulo.id'],
+            order: [['id']],
+        });
         return res.status(200).json({ data: modulos });
     } catch (err) {
         return res.sendStatus(500);
     }
-}
+};
 
 module.exports = { getModulos }
