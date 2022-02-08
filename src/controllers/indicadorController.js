d33a61113125ae7017eb3828daa2be27004c67d6
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-02-08 10:49:15 -0700

    Minor changes

--- src/controllers/indicadorController.js
+++ src/controllers/indicadorController.js
@@ -12,7 +12,6 @@ const {
 } = require("../models");
 const { Op } = require("sequelize");
 const { getPagination } = require("../utils/pagination");
-const { serviceGetIndicador } = require("../services/indicadorService");
 
 const sequelize = require("sequelize");
 const { generateCSV, generateJSON, generateXLSX, generatePDF } = require("../services/generadorArchivosService");
@@ -70,9 +69,66 @@ const getIndicador = async (req, res) => {
   try {
     const idIndicador = req.matchedData.idIndicador;
     const format = req.matchedData.format;
-    
-    const limit = 5;
-    const indicador = serviceGetIndicador(idIndicador, format);
+    const indicador = await Indicador.findOne({
+      where: {
+        id: idIndicador,
+      },
+      include: [
+        {
+          model: UnidadMedida,
+          required: true,
+          attributes: [],
+        },
+        {
+          model: Modulo,
+          required: true,
+          attributes: ["id", "temaIndicador", "color"],
+        },
+        {
+          model: CoberturaGeografica,
+          required: true,
+          attributes: ["nombre"],
+        },
+        {
+          model: Historico,
+          required: true,
+          attributes: ["anio", "valor", "fuente"],
+          limit: 5,
+          order: [["anio", "DESC"]],
+        },
+        {
+          model: Mapa,
+          required: false
+        },
+        {
+          model: Formula,
+          required: false,
+          include: [
+            {
+              model: Variable,
+              required: true,
+              include: [{
+                model: UnidadMedida,
+                required: true,
+              }],
+              attributes: ['nombre', 'nombreAtributo', 'dato', 'idUnidad', [sequelize.literal('"Formula->Variables->UnidadMedida"'), "Unidad"],],
+            }
+          ]
+        }
+      ],
+      attributes: [
+        "id",
+        "urlImagen",
+        "nombre",
+        "definicion",
+        "ultimoValorDisponible",
+        "anioUltimoValorDisponible",
+        "tendenciaActual",
+        "tendenciaDeseada",
+        "mapa",
+        [sequelize.literal('"UnidadMedida"."nombre"'), "Unidad"],
+      ],
+    });
     
     if (indicador === null) {
       return res.sendStatus(404);
@@ -166,14 +222,14 @@ const generateFile = (format, res, data) => {
       return (
             res.header('Content-disposition', 'attachment'),
             res.header('Content-Type', 'application/json'),
-            res.attachment(`${data.nombre}.csv`),
+            res.attachment(`${data.nombre}.json`),
             res.send(jsonFile));
     case 'csv':
       const csvData = generateCSV(data);
       return (
             res.header('Content-disposition', 'attachment'),
             res.header('Content-Type', 'application/json'),
-            res.attachment(`${data.nombre}.json`),
+            res.attachment(`${data.nombre}.csv`),
             res.send(csvData));
     case 'xlsx':
       const x = generateXLSX(res, data);
