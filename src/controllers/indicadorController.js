9af4b89838a334d1ade8f7a0e8572484379a7388
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-02-14 11:50:33 -0700

    Fixed limit on service to serve indicador information

--- src/controllers/indicadorController.js
+++ src/controllers/indicadorController.js
@@ -1,4 +1,5 @@
 const IndicadorService = require("../services/indicadorService")
+
 const {
   Indicador,
   Ods,
@@ -12,8 +13,10 @@ const {
   Variable,
 } = require("../models");
 const { Op } = require("sequelize");
+
 const { getPagination } = require("../utils/pagination");
 const sequelize = require("sequelize");
+
 const { generateCSV, generateJSON, generateXLSX, generatePDF } = require("../services/generadorArchivosService");
 
 
@@ -40,66 +43,8 @@ const getIndicador = async (req, res) => {
   try {
     const idIndicador = req.matchedData.idIndicador;
     const format = req.matchedData.format;
-    const indicador = await Indicador.findOne({
-      where: {
-        id: idIndicador,
-      },
-      include: [
-        {
-          model: UnidadMedida,
-          required: true,
-          attributes: [],
-        },
-        {
-          model: Modulo,
-          required: true,
-          attributes: ["id", "temaIndicador", "color"],
-        },
-        {
-          model: CoberturaGeografica,
-          required: true,
-          attributes: ["nombre"],
-        },
-        {
-          model: Historico,
-          required: true,
-          attributes: ["anio", "valor", "fuente"],
-          limit: 5,
-          order: [["anio", "DESC"]],
-        },
-        {
-          model: Mapa,
-          required: false
-        },
-        {
-          model: Formula,
-          required: false,
-          include: [
-            {
-              model: Variable,
-              required: true,
-              include: [{
-                model: UnidadMedida,
-                required: true,
-              }],
-              attributes: ['nombre', 'nombreAtributo', 'dato', 'idUnidad', [sequelize.literal('"Formula->Variables->UnidadMedida"'), "Unidad"],],
-            }
-          ]
-        }
-      ],
-      attributes: [
-        "id",
-        "urlImagen",
-        "nombre",
-        "definicion",
-        "ultimoValorDisponible",
-        "anioUltimoValorDisponible",
-        "tendenciaActual",
-        "tendenciaDeseada",
-        "mapa",
-        [sequelize.literal('"UnidadMedida"."nombre"'), "Unidad"],
-      ],
-    });
+
+    const indicador = await IndicadorService.getIndicador (idIndicador);
 
     if (indicador === null) {
       return res.sendStatus(404);
@@ -111,49 +56,11 @@ const getIndicador = async (req, res) => {
 
     return (res.status(200).json({ data: indicador, }))
 
-  } catch (err) {
+  } catch (err) { 
     console.log(err);
     return res.sendStatus(500);
   }
 };
-
-// Includes for inner join to filter list 
-const getIndicadorIncludes = ({ idFuente }) => {
-  const indicadorFilter = [];
-
-  indicadorFilter.push({
-    model: Ods,
-    required: true,
-    attributes: []
-  });
-
-  indicadorFilter.push({
-    model: CoberturaGeografica,
-    required: true,
-    attributes: []
-  });
-
-  indicadorFilter.push({
-    model: UnidadMedida,
-    required: true,
-    attributes: []
-  });
-
-  if (idFuente) {
-    indicadorFilter.push({
-      model: Fuente,
-      where: {
-        id: {
-          [Op.eq]: idFuente,
-        },
-      },
-    });
-  }
-
-  return indicadorFilter;
-};
-
-
 
 const generateFile = (format, res, data) => {
   switch (format) {
