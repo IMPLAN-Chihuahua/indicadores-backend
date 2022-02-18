553a04715782c53ca8ec9953b5da3266d666dec7
Miguel Valdez <56283510+MiquelVald@users.noreply.github.com>
2022-02-18 13:14:23 -0700

    Added new endpoint to retrieve indicadores based on user ID via token and respective tests

--- src/test/controllers/meControllerTest.js
+++ src/test/controllers/meControllerTest.js
@@ -12,6 +12,10 @@ const SALT_ROUNDS = 10;
 require('dotenv').config();
 const { TOKEN_SECRET } = process.env;
 
+const { Indicador } = require('../../models');
+const { anIndicador, aModulo } = require('../../utils/factories');
+
+
 describe('v1/me', function () {
   const token = jwt.sign({ sub: 1 }, TOKEN_SECRET, { expiresIn: '5h' });
 
@@ -22,6 +26,16 @@ describe('v1/me', function () {
   this.afterAll(function () {
     server.close();
   });
+
+    this.beforeAll(function () {
+        indicadoresFromUser = [
+            anIndicador(1),
+            anIndicador(1),
+            anIndicador(1),
+            anIndicador(1),
+            anIndicador(1)
+        ];
+    });
 
   it('Should return the profile information of a user with a token', function (done) {
     const findOneFake = sinon.fake.resolves(aUser(1));
@@ -54,6 +68,47 @@ describe('v1/me', function () {
         expect(res).to.have.status(401)
         done();
       });
+  });
+
+  describe('me/indicadores', function () {
+
+    it('Should return a list of indicadores based on a user', function (done) {
+      const findAndCountAllFakeIndicadores = sinon.fake.resolves({indicadores: indicadoresFromUser, total: indicadoresFromUser.length});
+      sinon.replace(Indicador, 'findAndCountAll', findAndCountAllFakeIndicadores);
+      chai.request(app)
+        .get('/api/v1/me/indicadores')
+        .set({ Authorization: `Bearer ${token}` })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(findAndCountAllFakeIndicadores.calledOnce).to.be.true;
+          expect(res.body.cantidadIndicadores).to.be.equal(indicadoresFromUser.length);
+          done();
+        });
+    });
+
+    it('Should not return a list of indicadores based on a user because of token expiration', function (done) {
+      const findAndCountAllFakeIndicadores = sinon.fake.rejects({indicadores: indicadoresFromUser, total: indicadoresFromUser.length});
+      sinon.replace(Indicador, 'findAndCountAll', findAndCountAllFakeIndicadores);
+      chai.request(app)
+        .get('/api/v1/me/indicadores')
+        .set({ Authorization: `Bearer notvalid` })
+        .end((err, res) => {
+          expect(res).to.have.status(403);
+          done();
+        });
+    }); 
+    
+    it('Should not return a list of indicadores based on a user because of token is not present', function (done) {
+      const findAndCountAllFakeIndicadores = sinon.fake.rejects({indicadores: indicadoresFromUser, total: indicadoresFromUser.length});
+      sinon.replace(Indicador, 'findAndCountAll', findAndCountAllFakeIndicadores);
+      chai.request(app)
+        .get('/api/v1/me/indicadores')
+        .end((err, res) => {
+          expect(res).to.have.status(401);
+          done();
+        });
+    });   
+    
   });
 
 });
