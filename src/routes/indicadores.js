4b62b3d18da708625874cbc4010061bcf5c8f8ac
Luis Cruz <luisccc99@gmail.com>
2022-03-15 15:44:19 -0700

    Update Swagger documentation, create separate components for Indicadores and move it to indicadores route file

--- src/routes/indicadores.js
+++ src/routes/indicadores.js
@@ -18,6 +18,155 @@ const { verifyJWT, verifyRoles } = require('../middlewares/auth');
 
 /**
  * @swagger
+ *   components:
+ *     schemas:
+ *       Indicador:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: intger
+ *             description: Identifier of an indicador
+ *             example: 1
+ *             readOnly: true
+ *           urlImagen:   
+ *             type: string 
+ *             description: Base url to display a map
+ *             example: http://example.com/map
+ *           codigo:    
+ *             type: string
+ *             description: Code for Indicator
+ *             example: '001'
+ *           nombre:
+ *             type: string
+ *             description: Name of the Indicator
+ *             example: Almacen de Carbono
+ *           definicion:
+ *             type: string
+ *             description: Detailed information of an Indicador
+ *             example: Lorem ipsum at dolor
+ *           ultimoValorDisponible:
+ *             type: string
+ *             description: Last value available for the Indicator
+ *           anioUltimoValorDisponible:
+ *             type: string
+ *             description: Year of the late information of an Indicador
+ *             example: 2019
+ *           tendenciaActual:
+ *             type: string
+ *             description: Tendency based on historical data
+ *             example: Ascendente
+ *             readOnly: true
+ *           tendenciaDeseada:  
+ *             type: string
+ *             description: Desired tendency
+ *             example: Ascendente
+ *           observaciones:
+ *             type: string
+ *             description: Notes about an Indicador
+ *           createdBy:
+ *             type: integer
+ *             description: Identifier of the user who created an Indicador
+ *             example: 1
+ *           updatedBy:
+ *             type: integer
+ *             description: Identifier of the user who updated an Indicador
+ *             example: 1
+ *           mapa:
+ *             type: object
+ *             $ref: '#/components/schemas/Mapa'
+ *           formula:
+ *             type: object
+ *             $ref: '#/components/schemas/Formula'
+ *           historicos:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/Historico'
+ *           idOds:
+ *             type: integer
+ *             description: Identifier of the ODS (Objectivo de Desarrollo Sostenible)
+ *             example: 1
+ *           idCobertura:
+ *             type: integer
+ *             description: Identifier of the coverage
+ *             example: 1
+ *           idUnidadMedida:
+ *             type: integer
+ *             description: Identifier of the unit of measure
+ *             example: 1
+ *       Formula:
+ *         type: object
+ *         properties:
+ *           ecuacion:
+ *             type: string
+ *             description: TODO
+ *           descripcion:
+ *             type: string
+ *             description: TODO
+ *           variables:
+ *             type: array
+ *             items:
+ *               $ref: '#/components/schemas/Variable'
+ *       Variable:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: integer
+ *             readOnly: true
+ *           nombre:
+ *             type: string
+ *             description: TODO
+ *           codigoAtributo:
+ *             type: string
+ *             description: TODO
+ *           nombreAtributo:
+ *             type: string 
+ *             description: TODO
+ *           dato:
+ *             type: string
+ *             description: TODO
+ *           idUnidad:
+ *             type: integer
+ *             description: TODO
+ *           anio:
+ *             type: integer
+ *             description: TODO
+ *       Historico:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: integer
+ *             readOnly: true
+ *           valor:
+ *             type: string
+ *           anio:
+ *             type: integer
+ *           fuente:
+ *             type: string     
+ *       Fuente:
+ *         type: object
+ *         properties:
+ *           id:
+ *             type: integer
+ *             readOnly: true
+ *           bibliografia:
+ *             type: string  
+ *       Mapa:
+ *         type: object
+ *         properties:
+ *           id:    
+ *             type: integer
+ *             description: TODO
+ *             readOnly: true
+ *           ubicacion:
+ *             type: string
+ *             description: TODO 
+ *           url:
+ *             type: string
+ *             description: TODO
+ */
+
+/**
+ * @swagger
  *   /indicadores/{idIndicador}:
  *     get:
  *       summary: Indicador object
