2de1bb732ec2d6914564bf9e75d7115255e6aa11
Luis Cruz <luisccc99@gmail.com>
2022-05-03 14:51:13 -0600

    Add formatting and fix some typos in Swagger documentation

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 const { login, generatePasswordRecoveryToken, handlePasswordRecoveryToken } = require('../controllers/authController');
 const { loginValidationRules, validate, tokenValidationRules } = require('../middlewares/validator')
