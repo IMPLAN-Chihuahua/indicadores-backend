c81d5a5a3417e25b6d069be730e92500ef8fee70
Luis Cruz <luisccc99@gmail.com>
2022-05-13 11:55:57 -0600

    - Add message when indicador with given id is not found. - Change status code if modulo is not updated (from 404 to 400) - Add middlewares to filter data in site enpoint (to fetch indicadores from the site) - Add try-catch when fetching modulos

--- src/controllers/indicadorController.js
+++ src/controllers/indicadorController.js
@@ -11,7 +11,7 @@ const getIndicador = async (req, res) => {
     const { idIndicador, format } = req.matchedData;
     const indicador = await IndicadorService.getIndicador(idIndicador, pathway);
     if (indicador === null) {
-      return res.sendStatus(404);
+      return res.status(404).send(`Indicador con id ${idIndicador} no encontrado`);
     }
 
     if (typeof format !== 'undefined') {
@@ -63,9 +63,7 @@ const getIndicadores = async (req, res) => {
   try {
     const { indicadores, total } = await IndicadorService.getIndicadores(page, perPage, req.matchedData, pathway);
     const totalPages = Math.ceil(total / perPage);
-    if (indicadores.length > 0) {
-      return res.status(200).json({ page: page, perPage: perPage, total: total, totalPages: totalPages, data: indicadores });
-    }
+    return res.status(200).json({ page, perPage, total, totalPages, data: indicadores });
   } catch (err) {
     return res.status(500).json(err.message);
   }
@@ -107,7 +105,7 @@ const updateIndicador = async (req, res) => {
 
     let fields = {};
     if (urlImagen) {
-      fields = { ...indicador, urlImagen: urlImagen };
+      fields = { ...indicador, urlImagen };
       console.log('tiene imagen');
     }
     else {
