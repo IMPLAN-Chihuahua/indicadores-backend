9e2dde5f0966cc5d87f06b5a02f12e25da99806f
Luis Cruz <luisccc99@gmail.com>
2022-06-01 14:23:38 -0600

    Fix indicadores filtering by catalogos

--- src/utils/pagination.js
+++ src/utils/pagination.js
@@ -1,8 +1,4 @@
-const getPagination = (queryParams) => {
-    const page = queryParams.page || 1;
-    const perPage = queryParams.perPage || 15;
-    return { page, perPage };
-}
+const getPagination = ({ page, perPage }) => ({ page: page || 1, perPage: perPage || 15 });
 
 const getPaginationModulos = (queryParams) => {
     const page = queryParams.page || 1;
