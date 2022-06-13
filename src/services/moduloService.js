97f421fe4ead11d0ca59cd2ef88fec460c95a51d
Luis Cruz <luisccc99@gmail.com>
2022-06-13 11:33:26 -0600

    Fix the creation of different files, and refactor some names

--- src/services/moduloService.js
+++ src/services/moduloService.js
@@ -32,32 +32,16 @@ const getModulos = async () => {
 
 const addModulo = async (modulo) => {
     try {
-        const {
-            temaIndicador,
-            observaciones,
-            activo,
-            codigo,
-            urlImagen,
-            color,
-        } = await Modulo.create(modulo);
-        return {
-            temaIndicador,
-            observaciones,
-            activo,
-            codigo,
-            urlImagen,
-            color,
-        }
-
+        const created = await Modulo.create(modulo);
+        return created;
     } catch (err) {
         return Promise.reject(new Error(`Error al crear modulo ${err.message}`));
     }
 };
 
-const updateModulo = async (id, { temaIndicador, observaciones, activo, codigo, urlImagen, color }) => {
+const updateModulo = async (id, values) => {
     try {
-        const affectedRows = await Modulo.update(
-            { temaIndicador, observaciones, activo, codigo, urlImagen, color },
+        const affectedRows = await Modulo.update({...values},
             { where: { id } }
         );
         return affectedRows > 0;
