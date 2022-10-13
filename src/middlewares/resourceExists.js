66069e630b9742dd4c6c2d012758fc63d17cd717
Luis Cruz <luisccc99@gmail.com>
2022-10-13 12:46:23 -0600

    Add middleware to check if a resource (Indicador, Usuario, Variable, etc.) exists

---
+++ src/middlewares/resourceExists.js
@@ -0,0 +1,26 @@
+const models = require('../models');
+
+const verifyResourceExists = (pkName, modelName) => async (req, res, next) => {
+  try {
+    const id = req.matchedData[pkName];
+    const result = await models[modelName].findOne({
+      where: { id },
+      raw: true,
+      attributes: [
+        [models.Sequelize.fn('COUNT', 'id'), 'count']
+      ],
+    })
+    if (result.count > 0) {
+      next();
+    } else {
+      return res.status(404).json({
+        status: 404,
+        message: `${modelName} with id ${id} was not found`
+      });
+    }
+  } catch (err) {
+    next(err)
+  }
+};
+
+module.exports = { verifyResourceExists }
