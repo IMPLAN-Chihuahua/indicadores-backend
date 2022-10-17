9df420d83889e0b15036ae7177f99f3cb9ce3ff7
Miguel Valdez <mike.281299@gmail.com>
2022-10-17 12:13:00 -0600

    Added controller to get information about usuarios - indicadores

--- src/controllers/usuarioIndicadorController.js
+++ src/controllers/usuarioIndicadorController.js
@@ -1,5 +1,6 @@
 const stream = require('stream');
 const UsuarioIndicadorService = require('../services/usuarioIndicadorService');
+const { getUsuariosByBulk } = require('../services/usuariosService');
 
 const createRelation = async (req, res, next) => {
     const { idIndicador, idUsuario, usuarios, desde, hasta, indicadores } = req.matchedData;
@@ -37,7 +38,33 @@ const createRelation = async (req, res, next) => {
     }
 }
 
+const getIndicadoresRelations = async (req, res, next) => {
+    const { page, perPage, order, sortBy } = req.matchedData;
+
+    try {
+        const { data } = await UsuarioIndicadorService.getUsuariosIndicadores(page, perPage, order, sortBy);
+
+        // Iterate through data and retrieve only their id. If the ID repeats, ommitting repeated values
+        const ownersIds = [...new Set(data.map(indicador => indicador.owner))];
+
+        const { usuarios } = await getUsuariosByBulk(ownersIds);
+
+        // Generate a new array with the data and the owner's name
+        const dataWithOwners = data.map(indicador => {
+            const owner = usuarios.find(usuario => usuario.id === indicador.owner);
+            return {
+                ...indicador,
+                owner: owner.nombres + ' ' + owner.apellidoPaterno
+            }
+        });
+
+        return res.status(200).json({ data: dataWithOwners });
+    } catch (err) {
+        next(err);
+    }
+}
 
 module.exports = {
-    createRelation
+    createRelation,
+    getIndicadoresRelations,
 }
