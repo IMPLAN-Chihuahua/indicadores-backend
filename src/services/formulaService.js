629502c0d0bdb0c1d612fa809681466832245f26
Luis Cruz <luisccc99@gmail.com>
2022-10-21 09:59:48 -0600

    Add routes to update formula values, and add variables to a formula

--- src/services/formulaService.js
+++ src/services/formulaService.js
@@ -1,9 +1,9 @@
-const { Formula, Variable } = require('../models');
+const { Formula, Variable, CatalogoDetail } = require('../models');
 
 /**
  * @returns formula and variables:
  *  - with a known formula id
- *  - from an indicador (formula of an indicador)
+ *  - or formula of an indicador
  */
 const getFormulaAndVariables = async (where) => {
   const { idFormula, idIndicador } = where;
@@ -13,13 +13,48 @@ const getFormulaAndVariables = async (where) => {
         ...(idFormula && { id: idFormula }),
         ...(idIndicador && { idIndicador }),
       },
-      include: {
-        model: Variable
-      }
+      include: [{
+        model: Variable,
+        include: [{
+          model: CatalogoDetail,
+          attributes: ['id', 'nombre']
+        }],
+      }]
     });
   } catch (err) {
     throw err;
   }
 }
 
-module.exports = { getFormulaAndVariables }
+/**
+ * 
+ * @param {int} idFormula  
+ * @param {*} values of a formula to update (ecuacion, descripcion, isFormula, etc.)
+ * @returns true if formula was updated sucessfully
+ */
+const updateFormula = async (idFormula, values) => {
+  try {
+    const affected = await Formula.update(values, {
+      where: {
+        id: idFormula
+      }
+    });
+    return affected > 0;
+  } catch (err) {
+    throw err;
+  }
+}
+
+const createFormula = async (values) => {
+  try {
+    const options = {};
+    if (values?.variables && values?.variables.length > 0) {
+      options.include = [Variable];
+    }
+    return await Formula.create(values, options);
+  } catch (err) {
+    throw err;
+  }
+}
+
+module.exports = { getFormulaAndVariables, updateFormula, createFormula }
