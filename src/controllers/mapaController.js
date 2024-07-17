9667f1e69eb37d8fb5df3089d62888b1540f669d
Luis Cruz <luisccc99@gmail.com>
2024-07-17 15:17:24 -0600

    Refactor controller functions remove calls to validate functions and let verifyUserCanPerformActionOnIndicador handle it, and remove try-catch blocks because promise based router catches any unhandled error

--- src/controllers/mapaController.js
+++ src/controllers/mapaController.js
@@ -1,6 +1,4 @@
 const { Mapa } = require('../models');
-const { validate } = require('../services/authService');
-const { getIdIndicadorRelatedTo } = require('../services/indicadorService');
 const { getImagePathLocation } = require('../utils/stringFormat');
 
 const getMapaOfIndicador = async (req, res, next) => {
@@ -13,46 +11,26 @@ const getMapaOfIndicador = async (req, res, next) => {
   }
 }
 
-const createMapa = async (req, res, next) => {
+const createMapa = async (req, res, _next) => {
   const { idIndicador, ...values } = req.matchedData;
   const image = getImagePathLocation(req)
-  const rol = req.rol;
-  const idUsuario = req.sub;
-  try {
-    return validate(
-      { rol, idUsuario, idIndicador },
-      async () => {
-        const created = await Mapa.create({ ...values, ...image });
-        return res.status(201).json({ data: { ...created.dataValues } });
-      },
-      () => res.status(403).send('No tiene permiso para agregar el mapa al indicador')
-    )
-  } catch (err) {
-    next(err)
-  }
+
+  const created = await Mapa.create({ ...values, ...image });
+
+  return res.status(201).json({ data: created });
+
 }
 
-const updateMapa = async (req, res, next) => {
+const updateMapa = async (req, res, _next) => {
   const { idMapa, ...values } = req.matchedData;
   const image = getImagePathLocation(req);
-  const rol = req.rol;
-  const idUsuario = req.sub;
-  try {
-    const idIndicador = await getIdIndicadorRelatedTo(Mapa, idMapa);
-    return validate(
-      { rol, idUsuario, idIndicador },
-      async () => {
-        const affectedRows = await Mapa.update(
-          { ...values, ...image }, {
-          where: { id: idMapa }
-        });
-        return affectedRows > 0 ? res.sendStatus(204) : res.sendStatus(400);
-      },
-      () => res.status(403).send('No tiene permiso para actualizar el mapa del indicador')
-    )
-  } catch (err) {
-    next(err)
-  }
+
+  await Mapa.update(
+    { ...values, ...image }, {
+    where: { id: idMapa }
+  });
+
+  return res.sendStatus(204)
 }
 
 
