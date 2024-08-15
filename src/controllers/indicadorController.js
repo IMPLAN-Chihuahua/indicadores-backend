0de7c618290fc134dc33570927da62098669654e
Luis Cruz <luisccc99@gmail.com>
2024-08-15 09:14:02 -0600

    Remove validation of tema when getting indicadores

--- src/controllers/indicadorController.js
+++ src/controllers/indicadorController.js
@@ -13,13 +13,15 @@ const getIndicador = async (req, res, next) => {
   const { idIndicador, format } = req.matchedData;
   try {
     const indicador = await IndicadorService.getIndicador(idIndicador, pathway);
-    const hasConflict = indicador.activo === 'NO' || indicador?.modulo.activo === 'NO';
-    if (hasConflict && pathway !== FRONT_PATH) {
+
+    if (!indicador.activo && pathway !== FRONT_PATH) {
       return res.status(409).json({ status: 409, message: `El indicador ${indicador.nombre} se encuentra inactivo` });
     }
+
     if (pathway === FILE_PATH) {
       return generateFile(format, res, indicador).catch(err => next(err));
     }
+    
     return (res.status(200).json({ data: indicador, navigation: { prev: indicador.prev, next: indicador.next } }));
   } catch (err) {
     next(err)
