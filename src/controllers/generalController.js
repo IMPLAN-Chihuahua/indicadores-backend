edba9eaa8b4f8bcbcae0ae5d66d19b6b8ba522f3
Luis Cruz <luisccc99@gmail.com>
2024-10-21 10:25:40 -0600

    Refactor reading of matched data

--- src/controllers/generalController.js
+++ src/controllers/generalController.js
@@ -1,21 +1,17 @@
 const generalServices = require('../services/generalServices');
 
 const getInformation = async (req, res, next) => {
-    const page = req.matchedData.page || 1;
-    const perPage = req.matchedData.perPage || 15;
-    const attributes = req.matchedData.attributes || {};
-    const id = req.matchedData.id || null;
-    const where = id === null ? {} : { id };
-    const { sortBy, order } = req.matchedData;
+    const { page = 1, perPage = 15, attributes = [], id = null, sortBy, order } = req.matchedData;
     const { model } = req;
+    const where = id === null ? {} : { id };
+
     try {
         const { information, total } = await generalServices.getInformation(page, perPage, attributes, where, sortBy, order, model);
-        const totalPages = Math.ceil(total / perPage);
 
         return res.status(200).json({
             page,
             perPage,
-            totalPages,
+            totalPages: Math.ceil(total / perPage),
             total,
             data: information
         });
