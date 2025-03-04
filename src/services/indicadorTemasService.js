b5362c442bda57c7fcc80ef64132e69c92a615c0
Luis Cruz <luisccc99@gmail.com>
2025-03-04 10:10:40 -0600

    Move function to assing indicador to temas to indicador temas service file

--- src/services/indicadorTemasService.js
+++ src/services/indicadorTemasService.js
@@ -1,45 +1,59 @@
 const models = require('../models');
 const {
-    IndicadorTema,
+  IndicadorTema,
 } = models;
 
 const updateIndicadorTemas = async (indicadorId, temas) => {
-    try {
-        const temasOnIndicador = await IndicadorTema.findAll({
-            where: {
-                idIndicador: indicadorId,
-            },
-            attributes: ['idTema']
-        });
+  try {
+    const temasOnIndicador = await IndicadorTema.findAll({
+      where: {
+        idIndicador: indicadorId,
+      },
+      attributes: ['idTema']
+    });
 
-        const existingTemasId = temasOnIndicador.map(tema => tema.idTema);
+    const existingTemasId = temasOnIndicador.map(tema => tema.idTema);
 
-        const temasToAdd = temas.filter(tema => !existingTemasId.includes(tema));
+    const temasToAdd = temas.filter(tema => !existingTemasId.includes(tema));
 
-        const temasToDelete = existingTemasId.filter(tema => !temas.includes(tema));
+    const temasToDelete = existingTemasId.filter(tema => !temas.includes(tema));
 
-        if (temasToAdd.length > 0) {
-            await IndicadorTema.bulkCreate(temasToAdd.map(tema => ({
-                idIndicador: indicadorId,
-                idTema: tema
-            })));
-        }
-
-        if (temasToDelete.length > 0) {
-            await IndicadorTema.destroy({
-                where: {
-                    idIndicador: indicadorId,
-                    idTema: temasToDelete
-                }
-            });
-        }
-
-        return true;
-    } catch (err) {
-        throw err;
+    if (temasToAdd.length > 0) {
+      await IndicadorTema.bulkCreate(temasToAdd.map(tema => ({
+        idIndicador: indicadorId,
+        idTema: tema
+      })));
     }
+
+    if (temasToDelete.length > 0) {
+      await IndicadorTema.destroy({
+        where: {
+          idIndicador: indicadorId,
+          idTema: temasToDelete
+        }
+      });
+    }
+
+    return true;
+  } catch (err) {
+    throw err;
+  }
 }
 
+const assignIndicadorToTemas = async (idIndicador, idTemas) => {
+  try {
+    await IndicadorTema.bulkCreate(
+      idTemas.map(idTema => ({ idTema, idIndicador })), {
+      validate: true,
+      ignoreDuplicates: true,
+    })
+  } catch (err) {
+    throw new Error(`No se pudo asignar indicador a temas: ${err.message}`)
+  }
+}
+
+
 module.exports = {
-    updateIndicadorTemas
+  updateIndicadorTemas,
+  assignIndicadorToTemas
 }
